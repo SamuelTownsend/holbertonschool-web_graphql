57f9182c33f9706e4d59ff47982b18d71546d550
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-04-23 18:52:42 -0500

    Updating JQUERY and react components

--- GraphQL_API/client/src/components/TaskList.js
+++ GraphQL_API/client/src/components/TaskList.js
@@ -1,26 +1,44 @@
-import {
-  useState,
-  //useEffect
-} from "react";
-// components
+import {useState} from "react";
+import {graphql} from 'react-apollo';
 import TaskDetails from './TaskDetails';
+import { getTasksQuery } from '../queries/queries';
+
 
 function TaskList(props) {
   const [state, setState] = useState({
     selected: null
   });
+  function displayTasks() {
+    console.log(props.data);
+    var data = props.data;
 
-  return ( <
-    div >
-    <
-    ul id = "task-list" > {
-
-    } <
-    /ul>  <
-    TaskDetails /
-    > < /
-    div >
+    if (data.loading) {
+      return (<div> Loading tasks... </div>);
+      }
+      else {
+        return data.tasks.map(task => {
+          return (
+            <li
+              key={task.id}
+              onClick={(e) => {
+              setState({
+                selected: task.id
+              });
+              }}
+            >
+              {task.title}
+            </li>
+            );
+            })
+        }
+     }
+  return ( 
+    <div>
+      <ul id = "task-list">{displayTasks()}</ul> 
+      <TaskDetails taskId={state.selected}/>
+    </div>
   );
 }
 
-export default TaskList;
+export default graphql(getTasksQuery)(TaskList);
+
